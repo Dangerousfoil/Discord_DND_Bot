449045cb26f527ee8b11002ade4e67d3c7354f79
Dangerousfoil <148723211+Dangerousfoil@users.noreply.github.com>
2023-10-24 14:30:43 -0500

    dictionary to database conversion file

---
+++ dict_db_conversion.py
@@ -0,0 +1,25 @@
+from tinydb import TinyDB
+
+# change database path to desired database
+db = TinyDB('databases/animal_database.json')
+# change database name to desired database
+db.default_table_name = 'Animal_Database'
+
+
+class DatabaseSetup:
+    def __init__(self):
+        self.db = db
+        # copy the dictionary into the square brackets
+        self.dict = []
+
+    def new_db_info(self):
+        for i in self.dict:
+            self.db.insert(i)
+            print('Adding Item...')
+        x = len(self.dict)
+        print(f'Complete.\n{x} Items Added To Database.')
+
+
+# Run the program it will copy the items from the dictionary to your database
+database = DatabaseSetup()
+database.new_db_info()
