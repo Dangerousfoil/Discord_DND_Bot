d1650d9513978f30d2ab12b08d627d0523d83483
Cyclone0225 <125210448+Cyclone0225@users.noreply.github.com>
2023-10-25 15:31:13 -0500

    shit dont work

--- mine.py
+++ mine.py
@@ -7,6 +7,7 @@ special_material_database = TinyDB('databases/special_material_database.json')
 special_material_database.default_table_name = 'Special_Material_Database'
 user = Query()
 
+
 class Mine(commands.Cog):
     """
     Simulates mining for metal ore in different biomes. Uses a percent chance to determine the
@@ -15,6 +16,7 @@ class Mine(commands.Cog):
     """
 
     def __init__(self, bot):
+        self.modifier_input = None
         self.client = bot
         self.biome = ''
         self.success_response = []
@@ -25,112 +27,101 @@ class Mine(commands.Cog):
 
     @commands.command(name='mine')
     async def mine_start(self, ctx):
-        # Gets biome from user & starting point for command
-        embed = discord.Embed(title='**Mining**',
-                              description="**Please enter the biome you're mining in:**\n\n"
-                                          "**-Arctic**\n**-Desert**\n**-Grassland**\n**-Woodland**"
-                                          "\n**-Tundra**", color=discord.Color.blue())
-        await ctx.reply(embed=embed)
-
-        # Performs check to make sure the bot is interacting with the user that called the command
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-
-        response = await self.client.wait_for('message', check=check)
-        self.biome = response.content.lower()
-        # Checks user input against biome list
-        if self.biome not in self.biome_options:
-            embed = discord.Embed(title='**Invalid Biome**',
-                                  description=f'**{self.biome.title()} is not a recognized biome.'
-                                              f' Please enter a valid biome.**',
-                                  color=discord.Color.red())
+        while True:
+            # Gets biome from user & starting point for command
+            embed = discord.Embed(title='**Mining**',
+                                  description="**Please enter the biome you're mining in:**\n\n"
+                                              "**-Arctic**\n**-Desert**\n**-Grassland**\n**-Woodland**"
+                                              "\n**-Tundra**", color=discord.Color.blue())
             await ctx.reply(embed=embed)
-        else:
-            await self.tool_confirmation(ctx)
+
+            # Performs check to make sure the bot is interacting with the user that called the command
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            response = await self.client.wait_for('message', check=check)
+            self.biome = response.content.lower()
+            # Checks user input against biome list
+            if self.biome not in self.biome_options:
+                embed = discord.Embed(title='**Invalid Biome**',
+                                      description=f'**{self.biome.title()} is not a recognized biome.'
+                                                  f' Please enter a valid biome.**',
+                                      color=discord.Color.red())
+                await ctx.reply(embed=embed)
+            else:
+                await self.tool_confirmation(ctx)
+                break
 
     async def tool_confirmation(self, ctx):
-        embed = discord.Embed(title='**Mining**',
-                              description='**Do you have a pickaxe?**\n\n**Yes/No**',
-                              color=discord.Color.blue())
-        await ctx.reply(embed=embed)
-
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-
-        response = await self.client.wait_for('message', check=check)
-        tool_input = response.content.lower()
-        # Tells user if that are able to harvest or not depending on response
-        match tool_input:
-            case 'yes':
-                await self.location_check(ctx)
-            case _:
-                embed = discord.Embed(title='**Unable To Mine**',
-                                      description="**You can't gather metal ore without a pickaxe."
-                                                  " Mining ends.**",
-                                      color=discord.Color.blue())
-                await ctx.reply(embed=embed)
-
-    async def strength_modifier(self, ctx):
-        # Gets strength or dexterity modifier from user as input
-        embed = discord.Embed(title='**Modifier**',
-                              description='**Please provide your Strength or Dexterity '
-                                          'modifier:**', color=discord.Color.blue())
-        await ctx.reply(embed=embed)
-
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-
-        response = await self.client.wait_for('message', check=check)
-        modifier = response.content
-        # Checks to make sure the user has entered a number and displays as an invalid input if not
-        try:
-            modifier = int(modifier)
-            return modifier
-        except ValueError:
-            embed = discord.Embed(title='**Invalid Input**',
-                                  description='**Invalid input, please provide a valid integer.**',
-                                  color=discord.Color.red())
+        while True:
+            embed = discord.Embed(title='**Mining**',
+                                  description='**Do you have a Pickaxe and are you near a '
+                                              f'harvestable source of metal?**\n\n**Yes/No**',
+                                  color=discord.Color.blue())
             await ctx.reply(embed=embed)
 
-    async def location_check(self, ctx):
-        # Asks the user if they are near a source of metal
-        embed = discord.Embed(title='**Mining**',
-                              description='**Are you near a harvestable source of metal?**\n\n'
-                                          '**Yes/No**', color=discord.Color.blue())
-        await ctx.reply(embed=embed)
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
 
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
+            response = await self.client.wait_for('message', check=check)
+            tool_input = response.content.lower()
+            # Tells user if that are able to harvest or not depending on response
+            match tool_input:
+                case 'yes':
+                    await self.strength_modifier(ctx)
+                    break
+                case _:
+                    embed = discord.Embed(title='**Unable To Mine**',
+                                          description="**You can't gather metal without proper"
+                                                      " tools or without being near a harvestable "
+                                                      "source of metal. "
+                                                      " Mining ends.**",
+                                          color=discord.Color.blue())
+                    await ctx.reply(embed=embed)
 
-        response = await self.client.wait_for('message', check=check)
-        location_input = response.content.lower()
-        # If the user is not near a source of metal end gathering
-        match location_input:
-            case 'yes':
-                await self.result(ctx)
-            case _:
-                embed = discord.Embed(title='**Unable To Gather**',
-                                      description='**You are not in the right location to gather '
-                                                  'resources. Mining Ends**.',
-                                      color=discord.Color.blue())
+    async def strength_modifier(self, ctx):
+        while True:
+            # Gets strength or dexterity modifier from user as input
+            embed = discord.Embed(title='**Modifier**',
+                                  description='**Please provide your Strength or Dexterity '
+                                              'modifier:**',
+                                  color=discord.Color.blue())
+            await ctx.reply(embed=embed)
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            response = await self.client.wait_for('message', check=check)
+            self.modifier_input = response.content
+            # Checks to make sure the user has entered a number and displays as an invalid input if not
+            try:
+                self.modifier_input = int(self.modifier_input)
+                break
+            except ValueError:
+                embed = discord.Embed(title='**Invalid Input**',
+                                      description='**Invalid input, please provide a valid integer.**',
+                                      color=discord.Color.red())
                 await ctx.reply(embed=embed)
+
+        await self.result(ctx)
 
     @staticmethod
     def special_metals(biome):
-        special_metal_options = special_material_database.search(user.Group.all([biome.title(), 'Metal']))
-        special_metal = random.choices(special_metal_options, weights = [0.05, 0.025, 0.0125])
+        special_metal_options = special_material_database.search(
+            user.Group.all([biome.title(), 'Metal']))
+        special_metal = random.choices(special_metal_options, weights=[0.05, 0.025, 0.0125])
         choice = special_metal[0]['Name']
         return choice
 
     async def result(self, ctx):
-        modifier = await self.strength_modifier(ctx)
+        self.modifier_input = await self.strength_modifier(ctx)
         extra_metal = self.special_metals(self.biome)
         # Displays the results for the mining attempts and displays the result
         num_gathered = 0
         success_count = 0
-        # Loops through success_chance and multiplies it by the modifier provided
+        # Loops through success_chance and multiplies it by the self.modifier_input provided
         for tier, success_chance in enumerate(self.success_tiers, start=1):
-            modified_chance = success_chance * modifier
+            modified_chance = success_chance * self.modifier_input
 
             if random.random() <= modified_chance:
                 success_count += 1
@@ -148,7 +139,7 @@ class Mine(commands.Cog):
                             value=f'*{num_gathered}x metal gathered*', inline=False)
             embed.add_field(name='**Special Material**',
                             value=f'*1x {extra_metal.title()} was found while you were mining.'
-                            '\n\n\nPlease contact your DM to add the metal amounts listed*')
+                                  '\n\n\nPlease contact your DM to add the metal amounts listed*')
             await ctx.reply(embed=embed)
         else:
             # Calls method that handles getting the responses for failures
