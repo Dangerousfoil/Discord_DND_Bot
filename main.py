574b526785f0cd664e3bff3802eeeaef786ac964
Dangerousfoil <christopher.platt@live.com>
2023-10-26 22:02:01 -0500

    changed token

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from api import bot_two_token
+from api import bot_one_token
 from fishing import Fishing
 from mine import Mine
 from hunting import Hunting
@@ -11,7 +11,7 @@ from discord.ext import commands
 version = discord.__version__
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix="*", intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 
 @client.event
@@ -26,4 +26,4 @@ async def on_ready():
 
 
 if __name__ == "__main__":
-    client.run(bot_two_token)
+    client.run(bot_one_token)
