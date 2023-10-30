eec9d2ba19549c1ff1bac7880b9b7bdea0f92adb
Dangerousfoil <christopher.platt@live.com>
2023-10-29 23:48:07 -0500

    variables changes

--- src/main.py
+++ src/main.py
@@ -9,16 +9,16 @@ from reagents import Reagent
 from discord.ext import commands
 
 # Setup of variables for Discord bot
-version = discord.__version__
-intents = discord.Intents.default()
-intents.message_content = True
-client = commands.Bot(command_prefix="!", intents=intents)
+VERSION = discord.__version__
+INTENTS = discord.Intents.default()
+INTENTS.message_content = True
+client = commands.Bot(command_prefix="!", intents=INTENTS)
 
 
 @client.event
 async def on_ready():
     # Handles actions when the bot first comes online
-    print(f"{client.user} Online. Running Version: {version}")
+    print(f"{client.user} Online. Running Version: {VERSION}")
     await client.add_cog(Fishing(client))
     await client.add_cog(Mine(client))
     await client.add_cog(Hunting(client))
